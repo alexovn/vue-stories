c250a9e4e8e26917a9a3346b087a6bc51637cbbd
Nikita Alexov <polarbeararmor@gmail.com>
2023-01-14 14:46:26 +0300

    update vite.config.js(add production mode)

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,47 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({command, mode}) => {
+
+  if (command === 'serve') {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `
+              @import "@/assets/styles/mixins.scss";
+              @import "@/assets/styles/vars.scss";
+            `
+          }
+        }
+      }
     }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "@/assets/styles/mixins.scss";
-          @import "@/assets/styles/vars.scss";
-        `
+  } else {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      base: '/vue-stories/',
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `
+              @import "@/assets/styles/mixins.scss";
+              @import "@/assets/styles/vars.scss";
+            `
+          }
+        }
       }
     }
   }
+
 })
