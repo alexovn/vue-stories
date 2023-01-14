73372a1dcd65559f36dd1d2ae99c3cd0e07a34bd
Nikita Alexov <polarbeararmor@gmail.com>
2023-01-14 14:56:25 +0300

    Revert "update vite.config.js(add production mode)"
    This reverts commit c250a9e4e8e26917a9a3346b087a6bc51637cbbd.
    
    I want to change branch for production build in the future update

--- vite.config.js
+++ vite.config.js
@@ -4,47 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
-
-  if (command === 'serve') {
-    return {
-      plugins: [vue()],
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-      },
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: `
-              @import "@/assets/styles/mixins.scss";
-              @import "@/assets/styles/vars.scss";
-            `
-          }
-        }
-      }
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  } else {
-    return {
-      plugins: [vue()],
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-      },
-      base: '/vue-stories/',
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: `
-              @import "@/assets/styles/mixins.scss";
-              @import "@/assets/styles/vars.scss";
-            `
-          }
-        }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/styles/mixins.scss";
+          @import "@/assets/styles/vars.scss";
+        `
       }
     }
   }
-
 })
